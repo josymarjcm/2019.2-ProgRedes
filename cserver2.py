a047662ed6df6ae2d68bbb02c280fd522d02fc40
Josymar Cortez <josymar.cortez15@gmail.com>
2019-08-11 19:35:38 -0300

    Adicionando servidor no repositório

---
+++ cserver2.py
@@ -0,0 +1,25 @@
+import socket
+
+host = '10.0.0.106'
+port = 55454
+
+tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+tcp_socket.bind((host, port))
+tcp_socket.listen(5)
+
+print('Aguardando Mensagens...\n\n')
+con, cliente = tcp_socket.accept()
+print('Conectado por: ', cliente)
+
+    
+arq = con.recv(512)
+arq = arq.decode('utf-8')
+
+fdin = open(arq,'w')
+while True:                
+    msg = con.recv(4096)
+    msg = msg.decode('utf-8')
+    fdin.write(msg)
+    if not msg: break
+    'print(cliente, ": ", msg)'
