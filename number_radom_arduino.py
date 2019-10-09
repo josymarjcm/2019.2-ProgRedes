fea32bf487d6bd35186a7cba74fd0df9ad833c28
josymarjcm <37883515+josymarjcm@users.noreply.github.com>
2019-10-09 16:01:12 -0300

    Update number_radom_arduino.py

--- number_radom_arduino.py
+++ number_radom_arduino.py
@@ -2,16 +2,18 @@ import time
 import serial
 from random import randint
 
-connect = serial.Serial('porta', 9600)
+connect = serial.Serial('COM10', 9600)
 
 l=[]
 i = 0
-while True:
+while i<100:
     a = randint(0,3)
     l.append(a)
-    connect.write(a)
-    connect.read()
-    time.sleep(1)   
+    a = str(a)
+
+    connect.write('a'.encode())
+    #connect.read()
+    time.sleep(2)   
     i+=1
 connect.close()
 print(l)
